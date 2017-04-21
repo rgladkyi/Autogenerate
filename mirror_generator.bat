65ad8b43c5c77fe6b02bf3a13e9929abed9a6c3c
rgladkyi <roman.gladkuy@gmail.com>
2017-04-21 22:22:12 +0300

    Added/Updated mirror_generator.bat.

--- mirror_generator.bat
+++ mirror_generator.bat
@@ -2,16 +2,27 @@
 set AUTO_GEN_SCRIPT_PATH=%1
 set MIRROR_DIR_PATH=%2
 
+set COMMAND_FOR_EXECUTE_WRAPPED=%3
+echo %COMMAND_FOR_EXECUTE_WRAPPED%
+rem Remove " "
+set COMMAND_FOR_EXECUTE=%COMMAND_FOR_EXECUTE_WRAPPED:"=%
+echo %COMMAND_FOR_EXECUTE%
+
+set FILE_TO_PROCESS_PATH=%4
+
 call create_directory_if_not_exist.bat %MIRROR_DIR_PATH%
 
+rem call ll "C:\CMDEX\Interfaces\*.bat" > %AUTO_GEN_SCRIPT_PATH%
+rem call sed -r -i "s/\.bat$//g" %AUTO_GEN_SCRIPT_PATH%
+
 echo Generating commands..
-call ll "C:\CMDEX\Interfaces\*.bat" > %AUTO_GEN_SCRIPT_PATH%
-call sed -r -i "s/\.bat$//g" %AUTO_GEN_SCRIPT_PATH%
-
+copy %FILE_TO_PROCESS_PATH% %AUTO_GEN_SCRIPT_PATH%
 call sed -r -i "s/^/PLACE_FOR_COMMAND_TO_EXECUTE/g" %AUTO_GEN_SCRIPT_PATH%
+call sed -i "s|PLACE_FOR_COMMAND_TO_EXECUTE|%COMMAND_FOR_EXECUTE%|g" %AUTO_GEN_SCRIPT_PATH%
 
-call sed -i "s|PLACE_FOR_COMMAND_TO_EXECUTE|echo. >> C:\CMDEX\Mirror\|g" %AUTO_GEN_SCRIPT_PATH%
-rem echo\. \>\> C\:\\CMDEX\\Mirror\\/g" %AUTO_GEN_SCRIPT_PATH%
+rem echo Deleting old mirror..
+rem del /q "C:\CMDEX\Mirror\*"
 
-echo Deleting old mirror..
-del /q "C:\CMDEX\Mirror\*"
+
+
+ 
